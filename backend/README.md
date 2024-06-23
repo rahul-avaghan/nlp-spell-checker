99d9101798c7b81ec36d628e56f5673e39661f0e
rahul avaghan <rahulbavaghan@gmail.com>
2024-06-23 22:15:32 +0200

    feat: add basic implementation

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,50 @@
-# nlp-application-assignment
+
+
+## Instuctions to run the app
+
+* Use python version `3.8.0`
+
+* set python env
+
+```
+python -m venv .venv   
+```
+
+```
+ source .venv/bin/activate  
+```
+
+* Install dependencies
+
+```
+pip install flask , textblob , autocorrect
+```
+* Run the app
+```
+flask --app app run --port 8000 --debugger
+```
+
+* **Endpoints
+Request**
+1. spellcheck
+```
+curl --location --request POST 'http://localhost:8000/spellcheck' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "text": "spell chec"
+}'
+```
+Response
+```
+{
+    "corrected_text": "spell check",
+    "original_text": "spell chec"
+}
+```
+
+2.For uploading file
+
+```
+curl --location --request POST 'http://127.0.0.1:8000/upload' \
+--form 'file=@"/Users/rahulavaghan/Desktop/y.txt"'
+```
---
+++ backend/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, redirect,jsonify, url_for, render_template, send_from_directory, send_file
+from werkzeug.utils import secure_filename
+import os
+from textblob import TextBlob
+import io
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt'}
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/spellcheck', methods=['POST'])
+def spellcheck():
+    data = request.get_json()
+    
+    if not data or 'text' not in data:
+        return jsonify({'error': 'No text provided'}), 400
+    
+    text = data['text']
+    blob = TextBlob(text)
+    corrected_text = blob.correct()
+    
+    return jsonify({
+        'original_text': text,
+        'corrected_text': str(corrected_text)
+    })
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    if file:
+        text = file.read().decode('utf-8')
+        blob = TextBlob(text)
+        corrected_text = blob.correct()
+        
+        # Create a BytesIO stream for the corrected text
+        corrected_stream = io.BytesIO()
+        corrected_stream.write(str(corrected_text).encode('utf-8'))
+        corrected_stream.seek(0)
+        
+        return send_file(corrected_stream, mimetype='text/plain', as_attachment=True, download_name='corrected_text.txt')
+    
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
